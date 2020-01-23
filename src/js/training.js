882506974da7737f801367f4c7a43265abb3f8fe
Aaron Montag <aaron.montag@imaginary.org>
2020-01-23 09:59:11 +0100

    Add basic button to switch into expert mode

--- src/js/training.js
+++ src/js/training.js
@@ -1,5 +1,6 @@
 currentSlide().onEnter = async (controller) => {
   var showpreviewpaint = false;
+  var expertmode = false;
   var istraining = true;
   const d = document.querySelector(".train");
 
@@ -13,6 +14,7 @@ currentSlide().onEnter = async (controller) => {
 
   var updateUI = () => {
     istraining = (controller.nn && controller.nn.training);
+    expertmode = d.querySelector('.expertmode').checked;
     if (showpreviewpaint) {
       d.querySelector(".simplenetwork").classList.remove("visible");
       d.querySelector(".advanced").classList.remove("visible");
@@ -21,13 +23,21 @@ currentSlide().onEnter = async (controller) => {
 
       d.querySelector(".menu").classList.add("drawmode");
     } else {
-      d.querySelector(".simplenetwork").classList.add("visible");
-      d.querySelector(".advanced").classList.remove("visible");
+      if(expertmode) {
+        d.querySelector(".simplenetwork").classList.remove("visible");
+        d.querySelector(".advanced").classList.add("visible");
+      } else {
+        d.querySelector(".simplenetwork").classList.add("visible");
+        d.querySelector(".advanced").classList.remove("visible");
+      }
+
       d.querySelector(".paint").classList.remove("visible");
       d.querySelector(".testit").innerHTML = "-&gt; Test the Network!";
 
       d.querySelector(".menu").classList.remove("drawmode");
     }
+
+    d.querySelector(".expertmode-on-off").innerHTML = expertmode ? "on" : "off";
 
     const pr = d.querySelector(".pause-resume");
     if (istraining) {
@@ -50,6 +60,10 @@ currentSlide().onEnter = async (controller) => {
     }
     updateUI();
   };
+
+  d.querySelector('.expertmode').onchange = () => {
+    updateUI();
+  }
 
   d.querySelector(".pause-resume").onclick = async () => {
     istraining = !istraining;
