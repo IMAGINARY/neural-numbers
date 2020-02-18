b4a553ba9c51b1d2a19f4e1d847f74d1c534381f
Aaron Montag <aaron.montag@imaginary.org>
2020-02-18 16:54:39 +0100

    Allways cancel advanced mode when returning

--- src/js/training.js
+++ src/js/training.js
@@ -4,8 +4,8 @@ currentSlide().onEnter = async (controller) => {
   var istraining = true;
   const d = document.querySelector(".train");
 
-  d.querySelector('.activate').classList.add("visible");
-  d.querySelector('.cancel').classList.add("remove");
+  d.querySelector('.modeswitch .activate').classList.add("visible");
+  d.querySelector('.modeswitch .cancel').classList.remove("visible");
   d.querySelector('.modelid').selectedIndex = 0;
   d.querySelector('.activation').selectedIndex = 0;
   d.querySelector('.optimizerid').selectedIndex = 0;
